c4c40ddc7fdcc915319e6cfd71956a946320336b
Salif Mehmed <mail@salif.eu>
2023-11-14 23:55:56 +0200

    v4.0.0

--- README.md
+++ README.md
@@ -1,17 +1,33 @@
 # hide-text
 
+Hide a secret text message inside a text message
+
 ## Install
 
 ```bash
 npm install -g @salifm/hide-text
 ```
 
-### or
+or
 
 ```bash
 npm install -g git+https://codeberg.org/salif/hide-text.git
 ```
 
-# License
+## Usage
+
+```bash
+hide-text-cli hide "public text" "secret text"
+hide-text-cli unhide "public text"
+```
+
+or
+
+```bash
+# Requires zenity
+hide-text
+```
+
+## License
 
 Licensed under the [MIT](./LICENSE) license
--- hide-text.js
+++ hide-text.js
@@ -6,32 +6,59 @@ const c_inv0 = String.fromCharCode(8203)
 const c_inv1 = String.fromCharCode(8204)
 const c_inv2 = String.fromCharCode(8205)
 
-switch (process.argv[2]) {
-	case "hide":
-		console.log(hide_text(process.argv[3], process.argv[4]))
-		break
-	case "unhide":
-		console.log(unhide_text(process.argv[3]))
-		break
-	case "version":
-		console.log(require("./package.json").version)
-		break
+function main() {
+	const cmd = process.argv[2] || "help"
+	const pkg = require("./package.json")
+
+	switch (cmd) {
+		case "hide":
+			try {
+				console.log(hide_text(process.argv[3] || "", process.argv[4] || ""))
+			} catch (err) {
+				console.error(err.toString())
+				process.exit(1)
+			}
+			break
+		case "unhide":
+			try {
+				console.log(unhide_text(process.argv[3] || ""))
+			} catch (err) {
+				console.error(err.toString())
+				process.exit(1)
+			}
+			break
+		case "info":
+			console.log("Version:", pkg.version)
+			console.log("Homepage:", pkg.homepage)
+			break
+		case "help":
+			console.log("Commands: hide, unhide, info, help")
+			break
+		default:
+			console.error("Unknown command:", process.argv[2])
+			process.exit(1)
+			break
+	}
 }
 
 function hide_text(pub, sec) {
+	if (pub.length < 2) {
+		throw new Error("The public text length must be at least 2")
+	}
 	let result = ""
 	for (let i = 0; i < sec.length; i++) {
 		result += sec.charCodeAt(i).toString(2) + "2"
 	}
-	result = result
+	return pub.charAt(0) + result
 		.replace(/0/g, c_inv0)
 		.replace(/1/g, c_inv1)
-		.replace(/2/g, c_inv2)
-	result = pub.charAt(0) + result + pub.substring(1)
-	return result
+		.replace(/2/g, c_inv2) + pub.substring(1)
 }
 
 function unhide_text(pub) {
+	if (pub.length < 2) {
+		throw new Error("The public text length must be at least 2")
+	}
 	let s = pub.substring(1, pub.lastIndexOf(c_inv2))
 	s = s.split(c_inv2).join("2").split(c_inv0).join("0").split(c_inv1).join("1")
 	s = s.split("2")
@@ -40,4 +67,8 @@ function unhide_text(pub) {
 		result += String.fromCharCode(parseInt(s[i], 2))
 	}
 	return result
+}
+
+if (typeof in_browser == "undefined") {
+	main()
 }
